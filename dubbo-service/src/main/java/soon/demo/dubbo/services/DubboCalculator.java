b712cf48ade6fa2e2114bfeb192f8ad063c923ef
soonsoft <soonz@aliyun.com>
2017-09-27 13:11:29 +0800

    Create DubboCalculator.java

---
+++ dubbo-service/src/main/java/soon/demo/dubbo/services/DubboCalculator.java
@@ -0,0 +1,31 @@
+package soon.demo.dubbo.services;
+
+import soon.demo.services.api.ICalculator;
+
+import javax.annotation.Resource;
+import java.math.BigDecimal;
+
+/**
+ * Created by zWX458959 on 2017/9/27.
+ */
+public class DubboCalculator implements ICalculator {
+
+    @Resource
+    private ICalculator caculator;
+
+    public BigDecimal add(BigDecimal num1, BigDecimal num2) {
+        return caculator.add(num1, num2);
+    }
+
+    public BigDecimal subtract(BigDecimal num1, BigDecimal num2) {
+        return caculator.subtract(num1, num2);
+    }
+
+    public BigDecimal multiply(BigDecimal num1, BigDecimal num2) {
+        return caculator.multiply(num1, num2);
+    }
+
+    public BigDecimal divide(BigDecimal num1, BigDecimal num2) {
+        return caculator.divide(num1, num2);
+    }
+}
