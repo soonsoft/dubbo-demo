b3cc1f58ee77bf9f945a38eaba830521980314c5
soonsoft <soonz@aliyun.com>
2017-09-27 13:10:24 +0800

    Create DubboRequestFilter.java

---
+++ dubbo-service/src/main/java/soon/demo/dubbo/filters/DubboRequestFilter.java
@@ -0,0 +1,13 @@
+package soon.demo.dubbo.filters;
+
+import com.alibaba.dubbo.rpc.*;
+
+/**
+ * Created by zWX458959 on 2017/9/27.
+ */
+public class DubboRequestFilter implements Filter {
+
+    public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
+        return null;
+    }
+}
