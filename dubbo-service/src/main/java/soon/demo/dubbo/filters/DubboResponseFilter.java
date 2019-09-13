0ff2270e4b344c683f5a847d9196db288046adba
soonsoft <soonz@outlook.com>
2019-09-13 21:58:37 +0800

    Update DubboResponseFilter.java

--- dubbo-service/src/main/java/soon/demo/dubbo/filters/DubboResponseFilter.java
+++ dubbo-service/src/main/java/soon/demo/dubbo/filters/DubboResponseFilter.java
@@ -3,9 +3,6 @@ package soon.demo.dubbo.filters;
 
 import com.alibaba.dubbo.rpc.*;
 
-/**
- * Created by zWX458959 on 2017/9/27.
- */
 public class DubboResponseFilter implements Filter {
 
     public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
